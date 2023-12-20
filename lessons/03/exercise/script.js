d1d712ec7a46ba0d4f9dd4959dfd2cad605bd15e
Clara <clarapachecodev@gmail.com>
2023-12-20 14:05:00 -0300

    Change the z property of the object position, sending the camera backwards, and making the cube show up

--- lessons/03/exercise/script.js
+++ lessons/03/exercise/script.js
@@ -26,6 +26,7 @@ const sizes = {
 // Camera
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
 scene.add(camera);
 
 // Renderer
@@ -33,3 +34,5 @@ scene.add(camera);
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 
 renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
