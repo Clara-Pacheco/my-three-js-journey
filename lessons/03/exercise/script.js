2198af30186db67a696f1d2dff024410d9edf927
Clara <clarapachecodev@gmail.com>
2023-12-20 09:52:24 -0300

    Create an object by instantiate the object's geometry and material and passing it as parameters to an mesh's instance, which will be added as a parameter to a scene

--- lessons/03/exercise/script.js
+++ lessons/03/exercise/script.js
@@ -3,3 +3,11 @@ import * as THREE from "three";
 // Instantiate a scene
 
 const scene = new THREE.Scene();
+
+// Object
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "red" });
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
