f8f419bd59dc9b368b75b9ff9e0966754f59d699
Clara <clarapachecodev@gmail.com>
2023-12-20 12:39:47 -0300

    Instatiate a render using WebGLRenderer passing as parameters an object with the canvas property and value canvas that was fetched at the top of the page

--- lessons/03/exercise/script.js
+++ lessons/03/exercise/script.js
@@ -27,3 +27,7 @@ const sizes = {
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 scene.add(camera);
+
+// Renderer
+
+const renderer = new THREE.WebGL1Renderer({ canvas: canvas });
