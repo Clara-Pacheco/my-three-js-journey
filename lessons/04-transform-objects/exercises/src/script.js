efddbd9005a515151c6701575785c21e05b8a38a
Clara <clarapachecodev@gmail.com>
2023-12-21 11:17:33 -0300

    Change mesh position using the position properties x,y and z, get the distance between the object's position and the center of the scene with the method length(), get the distance between the object and the Vector3 using the method distanceTo() and finally get the distance between the object and the camera also using the method distanceTo() and the camera.position to get the camera's position.

--- lessons/04-transform-objects/exercises/src/script.js
+++ lessons/04-transform-objects/exercises/src/script.js
@@ -1,39 +1,48 @@
-import * as THREE from 'three'
+import * as THREE from "three";
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+mesh.position.x = 0.7;
+mesh.position.y = -0.6;
+mesh.position.z = 1;
+scene.add(mesh);
+
+console.log(mesh.position.length());
+
+console.log(mesh.position.distanceTo(new THREE.Vector3(0, 1, 2)));
 
 /**
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
-}
+  width: 800,
+  height: 600,
+};
 
 /**
  * Camera
  */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+console.log(mesh.position.distanceTo(camera.position));
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
