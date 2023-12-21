60f876f96f8bc5f616893794e0b2163ed4ac8a30
Clara <clarapachecodev@gmail.com>
2023-12-21 11:26:30 -0300

    Use the method set() to update the position properties x,y and z at once

--- lessons/04-transform-objects/exercises/src/script.js
+++ lessons/04-transform-objects/exercises/src/script.js
@@ -12,17 +12,19 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
-mesh.position.x = 0.7;
-mesh.position.y = -0.6;
-mesh.position.z = 1;
+// mesh.position.x = 0.7;
+// mesh.position.y = -0.6;
+// mesh.position.z = 1;
+
+mesh.position.set(0.7, -0.6, 1);
 scene.add(mesh);
 
 console.log(mesh.position.length());
 
 console.log(mesh.position.distanceTo(new THREE.Vector3(0, 1, 2)));
 
-mesh.position.normalize();
-console.log(mesh.position.length());
+// mesh.position.normalize();
+// console.log(mesh.position.length());
 
 /**
  * Sizes
