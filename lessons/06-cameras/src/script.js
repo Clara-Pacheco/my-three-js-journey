34d8e90d665e627ea2e4fbcc91f282337faa52cb
Clara <clarapachecodev@gmail.com>
2024-01-02 12:46:48 -0300

    Comment the OrtographicCamera code, uncomment the PerspectiveCamera code, stop the object´s rotation and sent the camera a bit backward by increasing the z axes value

--- lessons/06-cameras/src/script.js
+++ lessons/06-cameras/src/script.js
@@ -23,25 +23,25 @@ const mesh = new THREE.Mesh(
 scene.add(mesh);
 
 // Camera
-// const camera = new THREE.PerspectiveCamera(
-//   45,
-//   sizes.width / sizes.height,
-//   0.1,
-//   100
-// );
-
-const aspectRatio = sizes.width / sizes.height;
-const camera = new THREE.OrthographicCamera(
-  -1 * aspectRatio,
-  1 * aspectRatio,
-  1,
-  -1,
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
   0.1,
   100
 );
-camera.position.x = 2;
-camera.position.y = 2;
-camera.position.z = 2;
+
+// const aspectRatio = sizes.width / sizes.height;
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRatio,
+//   1 * aspectRatio,
+//   1,
+//   -1,
+//   0.1,
+//   100
+// );
+// camera.position.x = 2;
+// camera.position.y = 2;
+camera.position.z = 3;
 camera.lookAt(mesh.position);
 scene.add(camera);
 
@@ -58,7 +58,7 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
   // Update objects
-  mesh.rotation.y = elapsedTime;
+  //   mesh.rotation.y = elapsedTime;
 
   // Render
   renderer.render(scene, camera);
