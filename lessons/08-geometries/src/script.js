134f92544939ecd02703c9590a90269df17c464e
Clara <clarapachecodev@gmail.com>
2024-01-10 11:07:09 -0300

    Add wireframe:true to the material to be able to see those triangles created by the subdivisions

--- lessons/08-geometries/src/script.js
+++ lessons/08-geometries/src/script.js
@@ -12,7 +12,10 @@ const scene = new THREE.Scene();
 
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+  wireframe: true,
+});
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
