3964489fd8f3aac556e872ef3ea862de59b22075
Clara <clarapachecodev@gmail.com>
2024-01-14 11:37:37 -0300

    Add the method close() to the folder to have it close by default

--- lessons/09-debugUI/src/script.js
+++ lessons/09-debugUI/src/script.js
@@ -36,6 +36,7 @@ scene.add(mesh);
 // gui.add(mesh.position, "y", -3, 3, 0.01);
 
 const cubeTweaks = gui.addFolder("Awesome cube");
+cubeTweaks.close();
 
 cubeTweaks.add(mesh.position, "y").min(-3).max(3).step(0.01).name("elevation");
 
