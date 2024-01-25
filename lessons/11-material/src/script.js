83090ba4b68915bbddee492d0abceec257a72a32
Clara <clarapachecodev@gmail.com>
2024-01-25 10:07:27 -0300

    Load all door textures contained inside of the static folder

--- lessons/11-material/src/script.js
+++ lessons/11-material/src/script.js
@@ -10,6 +10,18 @@ const canvas = document.querySelector("canvas.webgl");
 // Scene
 const scene = new THREE.Scene();
 
+// Loading textures
+
+const textureLoader = new THREE.TextureLoader();
+const doorColorTexture = textureLoader.load("../textures/door/color.jpg");
+const alphaTexture = textureLoader.load("../textures/door/alpha.jpg");
+const ambientOcclusionTexture = textureLoader.load(
+  "../textures/door/ambientOcclusion.jpg"
+);
+const doorHeightTexture = textureLoader.load("..textures/door/height.jpg");
+const metalnessTexture = textureLoader.load("..textures/door/metalness.jpg");
+const doorNormalTexture = textureLoader.load("../textures/door/normal.jpg");
+const roughnessTexture = textureLoader.load("..textures/door/roughness.jpg");
 /**
  * Sizes
  */
