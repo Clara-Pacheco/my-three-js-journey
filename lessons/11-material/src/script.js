9bfec0d4b8e0801b0a53ae22bd1c87e2d7a30a8e
Clara <clarapachecodev@gmail.com>
2024-01-26 19:34:03 -0300

    Import a new loader - `RBGELoader` from node_modules

--- lessons/11-material/src/script.js
+++ lessons/11-material/src/script.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import GUI from "lil-gui";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
 
 const gui = new GUI();
 /**
