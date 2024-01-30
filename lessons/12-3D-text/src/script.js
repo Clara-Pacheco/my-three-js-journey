7d6232e85042809dd3ab34cb1a04caf0a6045ab2
Clara <clarapachecodev@gmail.com>
2024-01-30 15:59:17 -0300

    Remove some triangles for performance matters changing the values of `curveSegments` and `bevelSegments`

--- lessons/12-3D-text/src/script.js
+++ lessons/12-3D-text/src/script.js
@@ -30,12 +30,12 @@ fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
     font: font,
     size: 0.5,
     height: 0.2,
-    curveSegments: 12,
+    curveSegments: 5,
     bevelEnabled: true,
     bevelThickness: 0.03,
     bevelSize: 0.02,
     bevelOffset: 0,
-    bevelSegments: 5,
+    bevelSegments: 4,
   });
 
   const textMaterial = new THREE.MeshBasicMaterial();
