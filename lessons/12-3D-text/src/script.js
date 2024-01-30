a112f38a636d8460134da585de130a5b13d1e936
Clara <clarapachecodev@gmail.com>
2024-01-30 18:05:25 -0300

    Comment all the `computeBoundingBox` and use the method `center` in the `textGeometry` to center the font

--- lessons/12-3D-text/src/script.js
+++ lessons/12-3D-text/src/script.js
@@ -45,13 +45,15 @@ fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
     bevelSegments: 4,
   });
 
-  textGeometry.computeBoundingBox();
-  console.log(textGeometry.boundingBox);
-  textGeometry.translate(
-    -(textGeometry.boundingBox.max.x - 0.02) * 0.5,
-    -(textGeometry.boundingBox.max.y - 0.02) * 0.5,
-    -(textGeometry.boundingBox.max.z - 0.03) * 0.5
-  );
+  //   textGeometry.computeBoundingBox();
+  //   console.log(textGeometry.boundingBox);
+  //   textGeometry.translate(
+  //     -(textGeometry.boundingBox.max.x - 0.02) * 0.5,
+  //     -(textGeometry.boundingBox.max.y - 0.02) * 0.5,
+  //     -(textGeometry.boundingBox.max.z - 0.03) * 0.5
+  //   );
+
+  textGeometry.center();
   const textMaterial = new THREE.MeshBasicMaterial();
   textMaterial.wireframe = true;
   const text = new THREE.Mesh(textGeometry, textMaterial);
