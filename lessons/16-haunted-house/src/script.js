e3ad4b2e016199b4189616b4abbee6aac5c34312
Clara <clarapachecodev@gmail.com>
2024-02-16 09:47:35 -0300

    Enconde the `doorColorTexture` in SRGBColorSpace

--- lessons/16-haunted-house/src/script.js
+++ lessons/16-haunted-house/src/script.js
@@ -24,15 +24,22 @@ scene.fog = fog;
  */
 const textureLoader = new THREE.TextureLoader();
 const doorColorTexture = textureLoader.load("../textures/door/color.jpg");
+doorColorTexture.colorSpace = THREE.SRGBColorSpace;
+
 const doorAlphaTexture = textureLoader.load("../textures/door/alpha.jpg");
+
 const ambientOcclusionTexture = textureLoader.load(
   "../textures/door/ambientOcclusion.jpg"
 );
+
 const doorHeightTexture = textureLoader.load("../textures/door/height.jpg");
+
 const doorNormalTexture = textureLoader.load("../textures/door/normal.jpg");
+
 const doorMetalnessTexture = textureLoader.load(
   "../textures/door/metalness.jpg"
 );
+
 const doorRoughnessTexture = textureLoader.load(
   "../textures/door/roughness.jpg"
 );
