1c923aec4ca41d4d380e48e113c6cad0262b5388
Clara <clarapachecodev@gmail.com>
2024-02-21 10:54:11 -0300

    Activate the `shadowMap` so that the shadows start working

--- lessons/16-haunted-house/src/script.js
+++ lessons/16-haunted-house/src/script.js
@@ -330,7 +330,7 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.setClearColor("#262837");
-
+renderer.shadowMap.enabled = true;
 /**
  * Animate
  */
