e87b7729c373e218947c63b6a827de52f4de760d
Clara <clarapachecodev@gmail.com>
2024-02-21 10:48:01 -0300

    Add the animation for the third ghost and more randomness to the radius used changing position of axies x and z

--- lessons/16-haunted-house/src/script.js
+++ lessons/16-haunted-house/src/script.js
@@ -351,6 +351,12 @@ const tick = () => {
   ghost2.position.z = Math.sin(ghost2Angle) * 5;
   ghost2.position.y = Math.sin(elapsedTime * 4) + Math.sin(elapsedTime * 2.5);
 
+  const ghost3Angle = -elapsedTime * 0.18;
+  ghost3.position.x =
+    Math.sin(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.32));
+  ghost3.position.z = Math.sin(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.5));
+  ghost3.position.y = Math.sin(elapsedTime * 4) + Math.sin(elapsedTime * 2.5);
+
   // Update controls
   controls.update();
 
